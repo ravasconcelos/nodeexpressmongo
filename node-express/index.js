d1bc828e3a18035fe0a07b13c005c4e09395af45
RODOLFO VASCONCELOS <rodolfo.vasconcelos@ericsson.com>
2019-08-25 16:20:53 -0400

    Assignment 1

--- node-express/index.js
+++ node-express/index.js
@@ -1,8 +1,9 @@
 const express = require('express'),
      http = require('http');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -14,6 +15,8 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 const server = http.createServer(app);
 
--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -25,4 +25,24 @@ dishRouter.route('/')
     res.end('Deleting all dishes');
 });
 
+dishRouter.route('/:dishId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send the dish ' + req.params.dishId + ' to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the dish id:' + req.params.dishId + ' name:' + req.body.name + ' details:' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes/:dishId');
+})
+.delete((req, res, next) => {
+    res.end('Deleting the dish ' + req.params.dishId);
+});
+
 module.exports = dishRouter;
