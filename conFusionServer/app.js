9995d9247e6b5728fcd52ff13e7a7800a7d500e4
RODOLFO VASCONCELOS <rodolfo.vasconcelos@ericsson.com>
2019-09-08 13:45:18 -0400

    Passport

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -11,6 +11,9 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
@@ -40,27 +43,23 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-  if(!req.session.user) {
+    if (!req.user) {
       var err = new Error('You are not authenticated!');
       err.status = 403;
-      return next(err);
-  }
-  else {
-    if (req.session.user === 'authenticated') {
-      next();
+      next(err);
     }
     else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+          next();
     }
-  }
 }
 
 app.use(auth);
